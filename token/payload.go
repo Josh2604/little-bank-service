cf09b629827983fdeb70c5dcebf021770c0f9014
Josh2604 <josh.an.bas@gmail.com>
2023-01-17 21:18:28 -0600

    feat: create and verify JWT and PASETO token

---
+++ token/payload.go
@@ -0,0 +1,45 @@
+package token
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+var ErrExpiredToken = errors.New("token has expired")
+var ErrInvalidToken = errors.New("token is invalid")
+
+// Payload contains the payload data of the token
+type Payload struct {
+	ID        uuid.UUID `json:"id"`
+	UserName  string    `json:"username"`
+	IssuedAt  time.Time `json:"issued_at"`
+	ExpiredAt time.Time `json:"expires_at`
+}
+
+// NewPayload creates a new token payload with a specific username and duration
+func NewPayload(username string, duration time.Duration) (*Payload, error) {
+	tokenID, err := uuid.NewRandom()
+	if err != nil {
+		return nil, fmt.Errorf("error generating random token: %v", err)
+	}
+
+	payload := &Payload{
+		ID:        tokenID,
+		UserName:  username,
+		IssuedAt:  time.Now(),
+		ExpiredAt: time.Now().Add(duration),
+	}
+
+	return payload, nil
+}
+
+// Valid checks if the token payload is valid or not
+func (payload *Payload) Valid() error {
+	if time.Now().After(payload.ExpiredAt) {
+		return ErrExpiredToken
+	}
+	return nil
+}
