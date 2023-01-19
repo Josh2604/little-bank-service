854c349f4aa1368c4622d9221fb396a10aa17048
Josh2604 <josh.an.bas@gmail.com>
2023-01-18 19:22:05 -0600

    feat:implementing JWT and PASETO Autentication

--- api/account_test.go
+++ api/account_test.go
@@ -101,7 +101,7 @@ func TestGetAccountAPI(t *testing.T) {
 			// 	Return(account, nil)
 
 			// start test server and send request
-			server := NewServer(store)
+			server := newTestServer(t, store)
 			recorder := httptest.NewRecorder()
 
 			url := fmt.Sprintf("/accounts/%d", tc.accountID)
